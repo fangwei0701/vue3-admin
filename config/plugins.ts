7f594c9bed960c81a0c51773b162770a7af2859e
fangwei <fangwei@xencio.com>
2023-05-09 17:56:19 +0800

    feat(app): 修改插件引入

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,8 +2,8 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import {
+  AntDesignVueResolver,
   TDesignResolver,
-  ViewUiResolver,
 } from "unplugin-vue-components/resolvers";
 
 export const getPlugins = () => {
@@ -12,10 +12,16 @@ export const getPlugins = () => {
     AutoImport({
       imports: ["vue", "vue-router"],
       dts: "src/auto-imports.d.ts",
-      resolvers: [TDesignResolver({ library: "vue-next" }), ViewUiResolver()],
+      resolvers: [
+        TDesignResolver({ library: "vue-next" }),
+        AntDesignVueResolver(),
+      ],
     }),
     Components({
-      resolvers: [TDesignResolver({ library: "vue-next" }), ViewUiResolver()],
+      resolvers: [
+        TDesignResolver({ library: "vue-next" }),
+        AntDesignVueResolver(),
+      ],
       dirs: ["src/components"],
       dts: "src/components.d.ts",
       directoryAsNamespace: false,
